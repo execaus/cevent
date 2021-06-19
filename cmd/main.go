44b064601ec74dcbd137594e9b9da8f323ea9893
execaus <execaus@gmail.com>
2021-06-20 00:25:07 +0500

    add config in project

--- cmd/main.go
+++ cmd/main.go
@@ -5,17 +5,28 @@ import (
 	"cevent/pkg/handler"
 	"cevent/pkg/repository"
 	"cevent/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	server := new(cevent.Server)
-	if err := server.Run("8000", handlers.InitRouters()); err != nil {
+	if err := server.Run(viper.GetString("port"), handlers.InitRouters()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module cevent
+
+go 1.16
+
+require github.com/gin-gonic/gin v1.7.2
