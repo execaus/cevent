b1814b6a5614ba643e1aedbb806d274f86ff77b4
execaus <execaus@gmail.com>
2021-06-20 13:04:12 +0500

    implemented basic architecture

--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,9 @@ import (
 	"cevent/pkg/repository"
 	"cevent/pkg/service"
 	"github.com/spf13/viper"
+	"github.com/subosito/gotenv"
 	"log"
+	"os"
 )
 
 func main() {
@@ -14,7 +16,23 @@ func main() {
 		log.Fatalf("error initializing configs %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	if err := gotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module cevent
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.2
+require (
+	github.com/gin-gonic/gin v1.7.2
+	github.com/jmoiron/sqlx v1.3.4
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/spf13/viper v1.8.0
+)
